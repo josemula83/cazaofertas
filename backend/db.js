c02a8d4ebe813f23bb04062bbb922eea34b7d420
JoseMula <josemula83@gmail.com>
2025-08-05 18:00:09 +0200

    Actualizando db.js

--- backend/db.js
+++ backend/db.js
@@ -2,18 +2,17 @@ const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("database.db");
 
 db.serialize(() => {
-db.run(
-  `CREATE TABLE IF NOT EXISTS links (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT,
-    url TEXT,
-    category TEXT,
-    discount INTEGER,
-    asin TEXT,
-    price REAL
-  )`
-);
-
+  db.run(`
+    CREATE TABLE IF NOT EXISTS links (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      title TEXT,
+      url TEXT,
+      category TEXT,
+      discount INTEGER,
+      asin TEXT,
+      price REAL
+    )
+  `);
 });
 
 module.exports = db;
