5acf093835e33d2fb0eb01b139008367490f4a44
JoseMula <josemula83@gmail.com>
2025-08-05 17:47:48 +0200

    conectar backend

--- backend/db.js
+++ backend/db.js
@@ -2,9 +2,18 @@ const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("database.db");
 
 db.serialize(() => {
-  db.run(
-    "CREATE TABLE IF NOT EXISTS links (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, url TEXT, category TEXT, discount INTEGER)"
-  );
+db.run(
+  `CREATE TABLE IF NOT EXISTS links (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    url TEXT,
+    category TEXT,
+    discount INTEGER,
+    asin TEXT,
+    price REAL
+  )`
+);
+
 });
 
 module.exports = db;
